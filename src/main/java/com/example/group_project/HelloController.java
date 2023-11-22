0beacbca5e1a261c1d372f1150f258716f684071
kevinGWD <kevingwd@hotmail.com>
2023-11-22 18:01:43 -0500

    GUI set

--- src/main/java/com/example/group_project/HelloController.java
+++ src/main/java/com/example/group_project/HelloController.java
@@ -2,16 +2,59 @@ package com.example.group_project;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private TextField address;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TextField name;
+    @FXML
+    private TextField phone;
+    @FXML
+    private TextField ownerID;
+    @FXML
+    private TextField carID;
+    @FXML
+    private TextField model;
+    @FXML
+    private TextField biuldYear;
+    @FXML
+    private TextField cost;
+    @FXML
+    private TextArea discription;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField make;
+    @FXML
+    private TextField carVIN;
+    @FXML
+    private ComboBox type;
+    @FXML
+    private DatePicker date;
+    @FXML
+    private Button addNew;
+    @FXML
+    private Button update;
+    @FXML
+    private Button findByOwner;
+    @FXML
+    private Button findByCarID;
+    @FXML
+    private Button findByDate;
+    @FXML
+    private TextField queryOwnerID;
+    @FXML
+    private TextField queryCarID;
+    @FXML
+    private DatePicker startDate;
+    @FXML
+    private DatePicker endDate;
+    @FXML
+    private TableView displayArea;
+
+
 
     public void addNewBtn(ActionEvent actionEvent) {
 
